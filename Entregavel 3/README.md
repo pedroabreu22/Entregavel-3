76b93133dd11166cff885c9aa4b790f49fe215e9
pedroabreu22 <82334078+pedroabreu22@users.noreply.github.com>
2022-11-03 17:15:44 -0300

    entregavel 3

---
+++ Entregavel 3/README.md
@@ -0,0 +1,2 @@
+# site-operacoes
+Atividade assíncrona envolvendo as disciplinas de Desenvolvimento de Software em Nuvem e Ambientes de Desenvolvimento de Software
---
+++ Entregavel 3/app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send('Oi mundo ^-^');
+});
+
+var port = 3001;
+
+//iniciando o processo do servidor
+app.listen(port, function () {
+    console.log(`App está rodandando no http://localhost:${port}/`);
+});
+
+function soma(a, b) {
+    return a + b;
+}
+
+app.post('/soma', function (req, res) {
+    var body = req.body;
+    var resultado = soma(body.a, body.b);
+
+    res.send(`A soma de ${body.a} e ${body.b} é ${resultado}`);
+});
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+app.post('/subtrair', function (req, res) {
+    var body = req.body;
+    var resultado = subtracao(body.a, body.b);
+
+    res.send(`A subtração de ${body.a} e ${body.b} é ${resultado}`);
+});
+
+function divisao(a, b) {
+    return a / b;
+}
+
+app.post('/divisao', function (req, res) {
+    var body = req.body;
+    var resultado = divisao(body.a, body.b);
+
+    res.send(`A divisão de ${body.a} por ${body.b} é ${resultado}`);
+});
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+app.post('/multiplicar', function (req, res) {
+    var body = req.body;
+    var resultado = multiplicacao(body.a, body.b);
+
+    res.send(`A multiplicação de ${body.a} por ${body.b} é ${resultado}`);
+});
